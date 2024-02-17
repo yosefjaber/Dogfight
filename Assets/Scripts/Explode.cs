2a1814daedc5dd2e23244705a847d44f92e6e7f4
Jaber <jaber@localhost-live.home>
2024-02-16 20:35:17 -0500

    yolo

--- Assets/Scripts/Explode.cs
+++ Assets/Scripts/Explode.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
 
 public class Explode : MonoBehaviour
 {
+    [Header("Explode effect")]
+    public GameObject explosion; 
+    public GameObject plane;
+    public GameObject leftGear;
+    public GameObject rightGear;
+    public GameObject planeBody;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,16 @@ public class Explode : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject != plane && other.gameObject != leftGear && other.gameObject != rightGear && other.gameObject != planeBody)
+        {
+            Debug.Log(other.gameObject.name);
+            PhotonNetwork.Instantiate(explosion.name, transform.position, Quaternion.identity);
+            // Optionally, destroy the bomb game object to simulate the explosion effect
+            PhotonNetwork.Destroy(gameObject);
+        }
     }
 }
