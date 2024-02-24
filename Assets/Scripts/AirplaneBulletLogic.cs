f5922e0b81a4b8f134995ce2cdbe7be31577df63
Tarik Jaber <tariknjaber@gmail.com>
2024-02-24 16:41:04 -0500

    Made the guns work and added explosions to the bullet impact also cleaned up some code

---
+++ Assets/Scripts/AirplaneBulletLogic.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class AirplaneBulletLogic : MonoBehaviour
+{
+    public GameObject bulletExplosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        PhotonNetwork.Instantiate(bulletExplosion.name, transform.position, Quaternion.identity);
+        PhotonNetwork.Destroy(gameObject);
+    }
+}
