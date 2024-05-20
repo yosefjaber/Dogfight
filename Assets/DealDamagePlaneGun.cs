ad3d44147aa64b7dd8798886c231a4299a8a0381
Yosef Jaber <yusefj816@gmail.com>
2024-05-20 18:00:01 -0400

    Added damage to the gun bullet

---
+++ Assets/DealDamagePlaneGun.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Pun.UtilityScripts;
+
+public class DealDamagePlaneGun : MonoBehaviour
+{
+    public int damage = 30;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.gameObject.GetComponent<Health>())
+        {
+            if(damage >= other.transform.gameObject.GetComponent<Health>().health)
+            {
+                //kill
+                RoomManager.instance.kills++;
+                RoomManager.instance.SetHashes();
+                PhotonNetwork.LocalPlayer.AddScore(100);
+            }
+
+            other.transform.gameObject.GetComponent<PhotonView>().RPC("TakeDamage", RpcTarget.All, damage);
+        }
+    }
+}
