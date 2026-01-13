3ba94750da78f5f4d3def8a435d709d442ae2073
Yosef Jaber <yusefj816@gmail.com>
2026-01-13 08:18:19 -0500

    lol

--- Assets/Scripts/BulletsLeftText.cs
+++ Assets/Scripts/BulletsLeftText.cs
@@ -9,6 +9,7 @@ public class BulletsLeftText : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Dynamic 
         UpdateBulletsLeft(80);
     }
 
